8890d3afcd9ce9ad8ca86bc2b7e236282504f532
Logon Aluno <logonrmlocal@fiap.com.br>
2023-03-09 09:38:48 -0300

    criação de ojeto -  classe Principal

--- src/Principal.java
+++ src/Principal.java
@@ -3,6 +3,16 @@ public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		//tipo nome; --> declarar variavel
+		int idade;
+		Pessoa p1 = new Pessoa();
+		p1.nome = "Ana Clara"; 
+		p1.sexo = "Fem";
+		p1.idade = 34;
+		
+		System.out.println("Pessoa: " + p1.nome + " tem " + p1.idade);
+				
 
 	}
 
